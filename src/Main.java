492378325437df97ec3adf46390e9141928306dd
Natalia Grishechko <existencia84@gmail.com>
2024-04-17 22:56:13 +0300

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        int num = factorial(6);
+        int num = FactorialCalculator.factorial(10);
         System.out.println(num);
     }
-
-    public static int factorial(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
 }
-
